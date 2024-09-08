a12aae06aed2fb996e1465cc74e204ffac7da466
赵晨炀 <904852749@qq.com>
2024-09-08 09:09:05 +0800

    feat: framework code for parsiong output

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -16,7 +16,7 @@ type Transaction struct {
 }
 
 func ParseTransaction(binary []byte) *Transaction {
-	transaction := Transaction{}
+	transaction := &Transaction{}
 	reader := bytes.NewReader(binary)
 	bufReader := bufio.NewReader(reader)
 
@@ -34,7 +34,40 @@ func ParseTransaction(binary []byte) *Transaction {
 	}
 	transaction.txInputs = transactionInputs
 
-	return nil
+	/*
+		0100000001{813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c
+		6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e
+		24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7
+		f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c
+		7b8138bd94bdd531d2e213bf016b278afeffffff}
+		{02}{a135ef0100000000}1976a914bc
+		3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4
+		bc762dd5423e332166702cb75f40df79fea1288ac{19430600}
+
+		outputcount: 0x02
+
+		output:
+		1. amount in satoshi 1/ 1,000,000,000 of bitcoin (8 bytes)
+		a135ef0100000000
+
+		2. ScriptPubKey => ScriptSig
+
+		locktime: 19430600
+	*/
+
+	// read output connts
+	outputs := ReadVarint(bufReader)
+	transactionOutputs := []*TransactionOutput{}
+	for i := 0; i < int(outputs.Int64()); i++ {
+		output := NewTransactionOutput(bufReader)
+		transactionOutputs = append(transactionOutputs, output)
+	}
+	// get last four byte for lock time
+	lockTimeBytes := make([]byte, 4)
+	bufReader.Read(lockTimeBytes)
+	transaction.lockTime = LittleEndianToBigInt(lockTimeBytes, LITTLE_ENDIAN_4_BYTES)
+
+	return transaction
 }
 
 func getInputCount(bufReader *bufio.Reader) *big.Int {
